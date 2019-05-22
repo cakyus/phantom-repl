0288a2fc1a6cd99852bb331edf724aa68a238133
cakyus <cakyus@gmail.com>
2019-05-22 22:52:18 +0700

    ! use JSON RPC

--- simpleserver.js
+++ simpleserver.js
@@ -11,59 +11,25 @@ var i;
 server = require('webserver').create();
 service = server.listen(port, function (request, response) {
 
-	console.log('Request: ' + request.url);
-
 	// parse request
 
-	if (request.url.substr(1,1) != '?'){
-		console.log('Response: 400 Bad Request');
-		response.statusCode = 400;
-		response.headers = { 'Cache': 'no-cache', 'Content-Type': 'text/plain'};
-		response.write('400 Bad Request');
-		response.close();
-		return false;
-	}
+	query = JSON.parse(request.postRaw);
 
-	// parse query
-
-	query = {};
-	queryLine = request.url.substr(2).split('&');
-	for (i = 0; i < queryLine.length ; i++){
-		queryItem = queryLine[i].split('=', 2);
-		if (queryItem[0] == 'version'){
-			query.version = queryItem[1];
-		}
-		if (queryItem[0] == 'method'){
-			query.method = queryItem[1];
-		}
-		if (queryItem[0] == 'params'){
-			query.params = queryItem[1];
-		}
-		if (queryItem[0] == 'id'){
-			query.id = queryItem[1];
-		}
-	}
-
-	console.log('Request-Method: '+query.method);
-
-	query.methods = query.method.split('.', 2);
+	console.log('Request-Data: '+JSON.stringify(query));
 
 	// object phantom
 
-	if (query.methods[0] == 'phantom'){
-
-		if (query.methods[1] == 'exit'){
-			response.statusCode = 200;
-			response.headers = { 'Cache': 'no-cache', 'Content-Type': 'text/plain' };
-			response.write('200 OK');
-			response.close();
-			phantom.exit();
-		}
+	if (query.method == 'phantom.exit'){
+		response.statusCode = 200;
+		response.headers = { 'Cache': 'no-cache', 'Content-Type': 'text/plain' };
+		response.write('200 OK');
+		response.close();
+		phantom.exit();
 	}
 
-	response.statusCode = 200;
+	response.statusCode = 400;
 	response.headers = { 'Cache': 'no-cache', 'Content-Type': 'text/plain' };
-	response.write('200 OK');
+	response.write('400 Bad Request');
 	response.close();
 
 });
---
+++ tests/phantom-exit.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ **/
+
+
+$options = array(
+	'http' => array(
+		  'method'  => 'POST'
+		, 'header'  => "Content-Type: application/x-www-form-urlencoded"
+		, 'content' => json_encode(array(
+			  'jsonrpc' => '2.0'
+			, 'method' => 'phantom.exit'
+			))
+	)
+);
+
+$context  = stream_context_create($options);
+$result = file_get_contents('http://127.0.0.1:4242/', false, $context);
+
+var_dump($result);
